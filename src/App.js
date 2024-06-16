c852fb09a42022ab1555e540ff0c64f4f88da99f
Y <yin.thu.aye0@gmail.com>
2024-06-16 18:26:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <CompA/>
-        <h5>I am A.</h5>
-        <h5> I am also B </h5>
       </header>
     </div>
   );

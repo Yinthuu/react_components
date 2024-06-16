e1573cfe32fd1c3be55ea94a2e1426966a8812ff
Yin ThuAye <yin.thu.aye0@gmail.com>
2024-06-16 15:53:32 -0400

    first commit

---
+++ src/ex_02_compoA.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ex_02_compoA = () => {
+    return(        
+    <div>
+        <h1>Hello, World!</h1>
+        <p>This is a simple React component.</p>
+      </div>
+    );
+};
+
+export default ex_02_compoA;
+

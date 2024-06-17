0d3884aaee71ad901dd87e115f4315f4111ae886
Yin ThuAye <yin.thu.aye0@gmail.com>
2024-06-17 13:32:02 -0400

    First change For 222.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <CompA/>
         <h6>I am A1.</h6>
+        <h6>I am A2.</h6>
       </header>
     </div>
   );
